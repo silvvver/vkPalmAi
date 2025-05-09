0b56d927d3ad906c53883ca2573e9018cd61fd48
silvvver <kerchflasher@gmail.com>
2025-05-09 11:53:49 +0300

    fix: clean up AppConfig imports and path

--- myPalmApp/src/AppConfig.js
+++ myPalmApp/src/AppConfig.js
@@ -1,16 +1,25 @@
-import vkBridge, { parseURLSearchParamsForGetLaunchParams } from '@vkontakte/vk-bridge';
-import { useAdaptivity, useAppearance, useInsets } from '@vkontakte/vk-bridge-react';
+// myPalmApp/src/AppConfig.js
+import vkBridge, {
+  parseURLSearchParamsForGetLaunchParams,
+  useAppearance,
+  useAdaptivity,
+  useInsets,
+} from '@vkontakte/vk-bridge-react';
 import { AdaptivityProvider, ConfigProvider, AppRoot } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
-import { transformVKBridgeAdaptivity } from './utils';
+import { transformVKBridgeAdaptivity } from './utils/transformVKBridgeAdaptivity';
 import App from './App';
-;
 
 export const AppConfig = () => {
+  // получаем настройки внешнего вида и отступы от VK Bridge
   const vkBridgeAppearance = useAppearance() || undefined;
   const vkBridgeInsets = useInsets() || undefined;
+
+  // адаптивность (sizeX/sizeY) из VK Bridge
   const adaptivity = transformVKBridgeAdaptivity(useAdaptivity());
+
+  // платформа из launchParams (desktop_web → vkcom)
   const { vk_platform } = parseURLSearchParamsForGetLaunchParams(window.location.search);
 
   return (
@@ -18,7 +27,7 @@ export const AppConfig = () => {
       colorScheme={vkBridgeAppearance}
       platform={vk_platform === 'desktop_web' ? 'vkcom' : undefined}
       isWebView={vkBridge.isWebView()}
-      hasCustomPanelHeaderAfter={true}
+      hasCustomPanelHeaderAfter
     >
       <AdaptivityProvider {...adaptivity}>
         <AppRoot mode="full" safeAreaInsets={vkBridgeInsets}>
